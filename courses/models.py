fc4aa3b138d170ec1c3be288655c0eb1281becaa
davood <mohamadbagherdavoodi@gmail.com>
2022-04-02 15:16:58 +0200

    [Del] categories to courses

--- courses/models.py
+++ courses/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
-class Material(models.Model):
+class Chapter(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=500)
 
@@ -16,7 +16,12 @@ class Course(models.Model):
     year = models.ForeignKey("categories.Year", null=True, blank=True, on_delete=models.SET_NULL)
 
 
-class Year(models.Model):
+class Faculty(models.Model):
     year_level = models.PositiveIntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(6)], default=1, verbose_name="Année")
     year_name = models.CharField(max_length=50, verbose_name="Nom de l'année (Primaire/Collège/Lycée)")
+
+class University(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(max_length=1000)
+    
