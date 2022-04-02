bbe62c89c7e94d651cb94fcedf7d4d8603784729
davood <mohamadbagherdavoodi@gmail.com>
2022-04-02 15:45:11 +0200

    [Add] Useradmin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+import users.models as models
 
-# Register your models here.
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'accountType')
+    list_filter = ('accountType',)
+    search_fields = ('username',)
+
+
+@admin.register(models.Parameter)
+class ParameterAdmin(admin.ModelAdmin):
+    list_display = ('study_time_per_day', 'study_days_per_week', 'study_bloc_size')
+    search_fields = ('study_time_per_day', 'study_days_per_week', 'study_bloc_size')
+
+
+@admin.register(models.StudentToCourse)
+class StudentToCourseAdmin(admin.ModelAdmin):
+    list_display = ('student', 'course', 'status', 'note', 'participation_to_the_course')
+    list_filter = ('status',)
+    search_fields = ('student', 'course')
