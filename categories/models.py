50cad212b335aadf9e9a77d1fa2e5629a5c94ca9
davood <mohamadbagherdavoodi@gmail.com>
2022-04-02 12:10:30 +0200

    [Add] Categories model

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Material(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(max_length=1000)
+    year = models.ForeignKey("categories.Year", on_delete=models.SET_NULL)
+
+
+class Year(models.Model):
+    year_level = models.PositiveIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(6)], default=1, verbose_name="Année")
+    year_name = models.CharField(_(""), max_length=50, verbose_name="Nom de l'année (Primaire/Collège/Lycée)")
