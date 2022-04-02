75b39973d6d9c86c434b905aebea2122c18361dc
davood <mohamadbagherdavoodi@gmail.com>
2022-04-02 23:20:28 +0200

    [Add] add/delete schedule

--- courses/urls.py
+++ courses/urls.py
@@ -6,4 +6,6 @@ import courses.views as views
 urlpatterns = [
     path('', views.dashboard, name="dashboard"),
     path('course/<int:course_id>', views.courses, name="course"),
+    path('add_schedule', views.add_schedule, name="add_schedule"),
+    path('delete_schedule/<int:id>', views.delete_schedule, name="delete_schedule"),
 ]
