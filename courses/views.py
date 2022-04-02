20252abb4f7310bdf7ffe1e02cfd8c4fb7d2da2c
davood <mohamadbagherdavoodi@gmail.com>
2022-04-02 23:20:45 +0200

    [Add] submitexam backend logic

--- courses/views.py
+++ courses/views.py
@@ -5,7 +5,8 @@ from django.shortcuts import get_object_or_404, reverse
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 import courses.models as models
-import 
+import users.models as umodels
+
 
 def dashboard(request):
     return render(request, 'home.html')
@@ -25,3 +26,12 @@ def rate(request):
         messages.error(request,"You can only use POST for this URL")
         return HttpResponseRedirect(reverse("home"))
     data = request.POST
+    course_data, _ = umodels.StudentToCourse.objects.get_or_create(student=request.user, course__id=data['course'])
+    print(data)
+    print(data['rating'])
+    course_data.rating = data['rating']
+    course_data.grade = data['grade']
+    course_data.status = data['status']
+    course_data.save()
+    messages.success(request, "Rating saved")
+    return HttpResponseRedirect(reverse("home"))
