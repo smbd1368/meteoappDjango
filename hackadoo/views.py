a1d246bb6c89de9fdfefa6c4bb201e1e544ede4c
davood <mohamadbagherdavoodi@gmail.com>
2022-04-02 23:20:50 +0200

    [Upd] home view

--- hackadoo/views.py
+++ hackadoo/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from courses.models import Schedule
+from users.models import StudentToCourse
 
 
 @login_required
 def home(request):
     user = request.user
-
-    return render(request, 'index.html', context={'user': user})
+    schedules = Schedule.objects.filter(user=user)
+    s2c_qs = StudentToCourse.objects.filter(student=user, status="running")
+    return render(request, 'index.html', context={'user': user, "schedules": schedules, "s2c_qs": s2c_qs})
