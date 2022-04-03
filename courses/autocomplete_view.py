5a33f1bcab11da14aae4f34be178b189d1d2a766
davood <mohamadbagherdavoodi@gmail.com>
2022-04-03 10:49:17 +0200

    lel

--- courses/autocomplete_view.py
+++ courses/autocomplete_view.py
@@ -1,15 +1,14 @@
 from dal import autocomplete
 
 from courses.models import Course
-
+from users.models import StudentToCourse
 
 class CourseAutocomplete(autocomplete.Select2QuerySetView):
     def get_queryset(self):
         if not self.request.user.is_authenticated:
-            return Country.objects.none()
+            return Course.objects.none()
 
-        qs = Course.objects.all()
-
+        qs = [x.course for x in StudentToCourse.objects.filter(student=self.request.user, status="running")]
         if self.q:
             qs = qs.filter(name__istartswith=self.q)
 
